ef418324a1a424f480827a86328a3af48e01cb69
lachezarSamanliev <58422300+lachezarSamanliev@users.noreply.github.com>
2021-01-08 14:11:17 +0200

    Update shopDatabase.php

--- data/shopDatabase.php
+++ data/shopDatabase.php
@@ -1,8 +1,8 @@
 <?php
  $dbhost = 'remotemysql.com:3306';
-             $dbuser = 'soKsKrr2BK';
-             $dbpass = 'fn726z1jGH';
-             $dbname = 'soKsKrr2BK';
+             $dbuser = '########';
+             $dbpass = '########';
+             $dbname = '########';
 
 
    
